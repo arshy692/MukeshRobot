92871807252dfff878214b46a15f6c6c3898a8a4
arshy692 <162564228+arshy692@users.noreply.github.com>
2024-07-09 01:04:20 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,14 +1,14 @@
 class Config(object):
     LOGGER = True
-    API_ID = 27383453
-    API_HASH = 4c246fb0c649477cc2e79b6a178ddfaa
-    TOKEN = 7081234335:AAENPFHX43yJbVK0x0p_KL3PQM3-YGMPqd4
-    OWNER_ID= 7081234335
+    API_ID = "27383453"
+    API_HASH = "4c246fb0c649477cc2e79b6a178ddfaa"
+    TOKEN = "7297897460:AAFlRiyc9yCPmR1dYAaNrxxkZdXJWS3spto"
+    OWNER_ID= "7081234335"
     
-    SUPPORT_CHAT = @arshuu_69
-    START_IMG = https://te.legra.ph/file/cdff5691af1ba33d28379.jpg
-    EVENT_LOGS = -1002195635749
-    MONGO_DB_URI= mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority
+    SUPPORT_CHAT = "arshuu_69"
+    START_IMG = "https://te.legra.ph/file/cdff5691af1ba33d28379.jpg"
+    EVENT_LOGS = (-1002195635749)
+    MONGO_DB_URI= "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
    
     DATABASE_URL = ""  # A sql database url from elephantsql.com
     CASH_API_KEY = (
